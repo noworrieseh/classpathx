c22a473698f742be82564f2cf2606f5b496f91c6
Nic Ferrier <nferrier@gnu.org>
2001-05-19 02:32:56 +0000

    changed the mime type handling so that there is no need for extra implementation classes.
    MimetypesFileNameMap now initializes itself and stores the mime types
    using a hashtable of MimeType's keyed by file-name extensions.
    
    
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@9 814015b8-e43b-4d26-bdac-3fba12e27a77

--- activation/source/javax/activation/MimeType.java
+++ activation/source/javax/activation/MimeType.java
@@ -1,47 +1,54 @@
-/********************************************************************
- * Copyright (c) Open Java Extensions, Andrew Selkirk  LGPL License *
- ********************************************************************/
+/*
+  GNU-Classpath Extensions: java bean activation framework
+  Copyright (C) 2000 Andrew Selkirk
 
+  For more information on the classpathx please mail: nferrier@tapsellferrier.co.uk
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
 package javax.activation;
 
-// Imports
+
 import java.io.*;
 
-/**
- * MIME Type.
+
+/** represents a MIME type as per RFC2046.
+ *
+ * @author Andrew Selkirk: aselkirk@mailandnews.com
  */
 public class MimeType
 implements Externalizable
 {
 
-  //-------------------------------------------------------------
-  // Variables --------------------------------------------------
-  //-------------------------------------------------------------
-
-  /**
-   * Primary type.
+  /** primary type.
    */
-  private String primaryType = null;
+  String primaryType = null;
 
-  /**
-   * Sub type
+  /** sub type
    */
-  private String subType = null;
+  String subType = null;
 
-  /**
-   * MIME Type parameters.
+  /** MIME Type parameters.
    */
-  private MimeTypeParameterList parameters = new MimeTypeParameterList();
+  MimeTypeParameterList parameters = new MimeTypeParameterList();
 
   private static final String TSPECIALS = "()<>@,;:\\\"/[]?=";
 
 
-  //-------------------------------------------------------------
-  // Initialization ---------------------------------------------
-  //-------------------------------------------------------------
-
-  /**
-   * Create MIME Type object from MIME Type entry
+  /** create MIME Type object from MIME Type entry.
+   *
    * @param rawdata MIME Type entry
    */
   public MimeType(String rawdata) 
@@ -49,10 +56,11 @@ implements Externalizable
     try 
     {
       parse(rawdata);
-    } catch (Exception e) 
+    }
+    catch (Exception e) 
     {
     }
-  } // MimeType()
+  }
 
   /**
    * Create MIME Type object from primary/sub
@@ -63,30 +71,21 @@ implements Externalizable
   {
     primaryType = primary;
     subType = sub;
-  } // MimeType()
+  }
 
   /**
    * Create empty MIME Type
    */
   public MimeType() 
   {
-  } // MimeType()
+  }
 
-
-  //-------------------------------------------------------------
-  // Public Accessor Methods ------------------------------------
-  //-------------------------------------------------------------
-
-  /**
-   * Get string representation of MIME Type.
-   * @returns String representation
+  /** @return canonical representation of the MIME type
+   *   this is usually: <i>primary type</i>/<i>subtype</i>; <i>parameters</i>
    */
   public String toString() 
   {
-
-    // Variables
     StringBuffer buffer;
-
     // Construct Type
     buffer = new StringBuffer();
     buffer.append(getBaseType());
@@ -95,156 +94,152 @@ implements Externalizable
       buffer.append("; ");
       buffer.append(parameters.toString());
     }
-
-    // Return String
     return buffer.toString();
+  }
 
-  } // toString()
-
-  /**
-   * Parse MIME Type entry.
+  /** parse MIME Type entry.
+   *
    * @param mimetype MIME Type entry
    * @throws MimeTypeParseException MIME type parsing exception
    */
-  private void parse(String mimetype) throws MimeTypeParseException 
+  private void parse(String mimetype)
+  throws MimeTypeParseException 
   {
-
-    // Variables
     int endIndex;
     int typeIndex;
     String type;
     String parameterList;
     String primary;
     String sub;
-
-    // Check for type and parameter separator
+    //check for type and parameter separator
     endIndex = mimetype.indexOf(";");
     if (endIndex == -1) 
-    {
-      endIndex = mimetype.length();
-    }
-
-    // Get Type Index
+    endIndex = mimetype.length();
+    //get Type Index
     type = mimetype.substring(0, endIndex);
     typeIndex = type.indexOf("/");
-
-    // Get Primary and Sub
+    //get Primary and Sub
     primary = type.substring(0, typeIndex);
     sub = type.substring(typeIndex + 1, endIndex);
-
-    // Set Primary and Sub Types
+    //set Primary and Sub Types
     setPrimaryType(primary);
     setSubType(sub);
-
     // Set Parameter List
     parameterList = mimetype.substring(endIndex);
     parameters = new MimeTypeParameterList(parameterList);
+  }
 
-  } // parse()
-
-  /**
-   * Get base type of MIME Type.
-   * @returns Base type
+  /** @return Base type
    */
   public String getBaseType() 
   {
     return getPrimaryType() + "/" + getSubType();
-  } // getBaseType()
+  }
 
-  /**
-   * Get value of MIME type parameter.
+  /** get value of MIME type parameter.
+   *
    * @param name Parameter name
-   * @returns Parameter value, or null
+   * @return Parameter value, or null
    */
   public String getParameter(String name) 
   {
     return parameters.get(name);
-  } // getParameter()
+  }
 
-  /**
-   * Get parameters object.
-   * @returns Parameter object
+  /** @return Parameter object
    */
   public MimeTypeParameterList getParameters() 
   {
     return parameters;
-  } // getParameters()
+  }
 
-  /**
-   * Get primary of MIME Type.
-   * @returns Primary type
+  /** remove the specified parameter.
+   *
+   * @param name Parameter name
+   */
+  public void removeParameter(String name) 
+  {
+    parameters.remove(name);
+  }
+
+  /** set the MIME type parameter.
+   *
+   * @param name Parameter name
+   * @param value Parameter value
+   */
+  public void setParameter(String name, String value) 
+  {
+    parameters.set(name, value);
+  }
+
+  /** @return Primary type
    */
   public String getPrimaryType() 
   {
     return primaryType;
-  } // getPrimaryType()
+  }
 
-  /**
-   * Get sub of MIME Type.
-   * @returns Sub type
+  /** @return Sub type
    */
   public String getSubType() 
   {
     return subType;
-  } // getSubType()
+  }
 
-  /**
-   * Determine if token is character.
-   * @param token Character token
-   * @returns true if valid, false otherwise
-   */
-  private static boolean isTokenChar(char token) 
-  {
-    return false; // TODO
-  } // isTokenChar()
-
-  /**
-   * Determine if token is valid
-   * @param token Token
-   * @returns true if valid, false otherwise
-   */
-  private boolean isValidToken(String token) 
-  {
-    return false; // TODO
-  } // isValidToken(0
-
-  /**
-   * Determine if raw MIME Type entry matches this MIME Type.
-   * @param rawdata MIME type entry
-   * @returns true if matches, false otherwise
+  /** set the primary type of MIME type.
+   *
+   * @param primary Primary type
    * @throws MimeTypeParseException Parsing exception occurred
    */
-  public boolean match(String rawdata) throws MimeTypeParseException 
+  public void setPrimaryType(String primary)
+  throws MimeTypeParseException 
   {
+    primaryType = primary;
+  }
 
-    // Variables
+  /** set the sub type of MIME type.
+   *
+   * @param sub Sub type
+   * @throws MimeTypeParseException Parsing exception occurred
+   */
+  public void setSubType(String sub)
+  throws MimeTypeParseException 
+  {
+    subType = sub;
+  }
+
+  /** determine if raw MIME Type entry matches this MIME Type.
+   *
+   * @param rawdata MIME type entry
+   * @return true if matches, false otherwise
+   * @throws MimeTypeParseException Parsing exception occurred
+   */
+  public boolean match(String rawdata)
+  throws MimeTypeParseException 
+  {
     MimeType mimeType;
-
     // Construct Mime Type
     mimeType = new MimeType(rawdata);
-
     // Match Mime Type
     return match(mimeType);
+  }
 
-  } // match()
-
-  /**
-   * Determine if MIME Type matches this MIME Type.
+  /** determine if MIME Type matches this MIME Type.
+   *
    * @param type MIME type
-   * @returns true if matches, false otherwise
+   * @return true if matches, false otherwise
    */
   public boolean match(MimeType type) 
   {
-    if (getPrimaryType().equals(type.getPrimaryType()) == true &&
-	getSubType().equals(type.getSubType()) == true) 
-    {
-      return true;
-    }
-    return false;
-  } // match()
+    return (getPrimaryType().equals(type.getPrimaryType())
+	    && getSubType().equals(type.getSubType()))
+  }
 
-  /**
-   * Read external.  Part of serialization.
+
+  //externalizable implementation
+
+  /** Read external.  Part of serialization.
+   *
    * @param input Object input
    */
   public void readExternal(ObjectInput input)
@@ -252,74 +247,22 @@ implements Externalizable
   {
     try 
     {
-      parse((String) input.readObject());
-    } catch (MimeTypeParseException e) 
+      parse((String)input.readObject());
+    }
+    catch (MimeTypeParseException e) 
     {
     }
-  } // readExternal()
+  }
 
-  /**
-   * Remove parameter.
-   * @param name Parameter name
-   */
-  public void removeParameter(String name) 
-  {
-    parameters.remove(name);
-  } // removeParameter()
-
-  /**
-   * Set MIME type parameter.
-   * @param name Parameter name
-   * @param value Parameter value
-   */
-  public void setParameter(String name, String value) 
-  {
-    parameters.set(name, value);
-  } // setParameter()
-
-  /**
-   * Set primary type of MIME type.
-   * @param primary Primary type
-   * @throws MimeTypeParseException Parsing exception occurred
-   */
-  public void setPrimaryType(String primary)
-  throws MimeTypeParseException 
-  {
-
-    // Check Type
-    //TODO
-
-    // Set Primary Type
-    primaryType = primary;
-
-  } // setPrimaryType()
-
-  /**
-   * Set sub type of MIME type.
-   * @param sub Sub type
-   * @throws MimeTypeParseException Parsing exception occurred
-   */
-  public void setSubType(String sub)
-  throws MimeTypeParseException 
-  {
-
-    // Check SubType
-    // TODO
-
-    // Set Sub Type
-    subType = sub;
-
-  } // getSubType()
-
-  /**
-   * Write external.  Part of serialization.
+  /** write external.  Part of serialization.
+   *
    * @param output Object output
    * @throws IOException IO exception occurred
    */
-  public void writeExternal(ObjectOutput output) throws IOException 
+  public void writeExternal(ObjectOutput output)
+  throws IOException 
   {
     output.writeObject(toString());
-  } // writeExternal()
+  }
 
-
-} // MimeType
+}
--- activation/source/javax/activation/MimetypesFileTypeMap.java
+++ activation/source/javax/activation/MimetypesFileTypeMap.java
@@ -1,41 +1,143 @@
-/********************************************************************
- * Copyright (c) Open Java Extensions, Andrew Selkirk  LGPL License *
- ********************************************************************/
+/*
+  GNU-Classpath Extensions: javamail
+  Copyright (C) 2000 2001  Andrew Selkirk, Nic Ferrier
 
+  For more information on the classpathx please mail: nferrier@tapsellferrier.co.uk
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
 package javax.activation;
 
-// Imports
-import java.io.*;
-import oje.activation.registries.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.io.IOException;
+import java.util.Hashtable;
 import java.util.Properties;
 
-/**
- * MIME Types File Type Map.
+
+/** maps MIME types to "filename" extensions.
+ * The entire MIME type registry for an application can either be made
+ * from the contents of a stream (programmatic registry) or it can be
+ * constructed from a number of <code>mime.type</code> files in
+ * different locations.
+ *
+ * <h4>MIME registry file locations</h4>
+ * <p>The following files make up the default MIME registry:
+ * <ul>
+ * <li>~/.mime.types
+ * <li><i>java.home</i>/lib/mime.types
+ * </ul>
+ * and if the application is stored in a jar file:
+ * <ul>
+ * <li>/META-INF/mime.types
+ * <li>/META-INF/mimetypes.default
+ * </ul>
+ * </p>
+ *
+ * @author Andrew Selkirk: aselkirk@mailandnews.com
+ * @author Nic Ferrier: nferrier@tapsellferrier.co.uk
  */
 public class MimetypesFileTypeMap
 extends FileTypeMap 
 {
 
-  //-------------------------------------------------------------
-  // Variables --------------------------------------------------
-  //-------------------------------------------------------------
+  /** the standard mime type registry.
+   */
+  private static Hashtable defDB = null;
 
-  private static MimeTypeFile defDB = null;
-  private MimeTypeFile[] DB = null;
+  /** the default MIME type.
+   */
+  private static String defaultType = "application/octet-stream";
+
+  /** the full registry.
+   * Each index of the array is a hash of mime types read
+   * from some location. If parsing the contents of the location
+   * causes an error then the appropriate index will contain
+   * an empty hashtable.
+   */
+  private Hashtable[] DB = null;
+
+  /** index into the registry.
+   */
   private static final int PROG = 0;
   private static final int HOME = 1;
   private static final int SYS = 2;
   private static final int JAR = 3;
   private static final int DEF = 4;
-  private static String defaultType = "application/octet-stream";
 
 
-  //-------------------------------------------------------------
-  // Initialization ---------------------------------------------
-  //-------------------------------------------------------------
+  /** create default MIME Types registry.
+   */
+  public MimetypesFileTypeMap() 
+  {
+    DB = new Hashtable[5];
+    Properties properties=System.getProperties();
+    String sep=properties.getProperty("file.separator");
+    //init programmatic entries
+    DB[PROG] = new Hashtable();
+    //the user's mime types
+    try
+    {
+      File userHome=new File(properties.getProperty("user.home")+sep+".mime.types");
+      DB[HOME] = loadMimeRegistry(new FileReader(userHome));
+    }
+    catch(Exception e)
+    {
+      DB[HOME]=new Hashtable();
+    }
+    //the system mime types
+    try
+    {
+      File javaHome=new File(properties.getProperty("java.home")+sep+"mime.types");
+      DB[SYS] = loadMimeRegistry(new FileReader(javaHome));
+    }
+    catch(Exception e)
+    {
+      DB[SYS]=new Hashtable();
+    }
+    //a possible jar-file local copy of the mime types
+    try
+    {
+      String resource="META-INF" + separator + "mime.types";
+      InputStream str=getClass().getClassLoader().getResourceAsStream(resource);
+      DB[JAR]=loadMimeRegistry(new InputStreamReader(stream));
+    }
+    catch(Exception e)
+    {
+      DB[JAR]=new Hashtable();
+    }
+    //the default providers... obtained from a possible META-INF/ location
+    try
+    {
+      String resource="META-INF" + separator + "mimetypes.default";
+      InputStream str=getClass().getClassLoader().getResourceAsStream(resource);
+      DB[DEF]=loadMimeRegistry(new InputStreamReader(stream));
+    }
+    catch(Exception e)
+    {
+      DB[DEF]=new Hashtable();
+    }
+  }
 
-  /**
-   * Create MIME Types files type map with entries from stream.
+  /** create MIME Types registry from the stream.
+   *
    * @param stream MIME Types file map formatted stream
    */
   public MimetypesFileTypeMap(InputStream stream) 
@@ -43,157 +145,160 @@ extends FileTypeMap
     this();
     try 
     {
-      DB[PROG] = new MimeTypeFile(stream);
+      DB[PROG] = loadMimeRegistry(stream);
     }
     catch (Exception e) 
     {
-      DB[PROG] = null;
     }
-  } // MimetypesFileTypeMap()
-  
-  /**
-   * Create default MIME Types file type map.
-   */
-  public MimetypesFileTypeMap() 
-  {
-    // Variables
-    String separator;
-    Properties properties;
-    // Initialize
-    DB = new MimeTypeFile[5];
-    properties = System.getProperties();
-    separator = properties.getProperty("file.separator");
-    // Initialize Programmed Entries
-    DB[PROG] = new MimeTypeFile();
-    // Initialize User MimeTypes (~/.mime.types)
-    DB[HOME] = loadFile(properties.getProperty("user.home")+separator + ".mime.types");
-    // Initialize Java MimeTypes (<java-home>/lib/mime.types)
-    DB[SYS] = loadFile(properties.getProperty("java.home")
-		       +separator + "lib" + separator + "mime.types");
-    // Initialize Resource (META-INF/mime.types)
-    DB[JAR] = loadResource("META-INF" + separator + "mime.types");
-    // Initialize Default Resource (META-INF/mimetypes.default)
-    DB[DEF] = loadResource("META-INF" + separator + "mimetypes.default");
-  } // MimetypesFileTypeMap()
+  }
 
-  /**
-   * Create MIME Types files type map with entries from file
-   * @param mimeTypeFileName MIME Types file map formatted file
+  /** create MIME Types registry with entries from file
+   *
+   * @param mimeTypeFileName MIME Types file map formatted file name
    */
   public MimetypesFileTypeMap(String mimeTypeFileName) 
   {
     this();
     try 
     {
-      DB[PROG] = new MimeTypeFile(mimeTypeFileName);
+      DB[PROG] = loadMimeRegistry(new FileInputStream(mimeTypeFileName));
     }
     catch (Exception e) 
     {
     }
-  } // MimetypesFileTypeMap()
+  }
 
 
-  //-------------------------------------------------------------
-  // Public Accessor Methods ------------------------------------
-  //-------------------------------------------------------------
-  
-  /**
-   * Get content type of file.
+  /** get content type of file.
+   *
    * @param file File to check
-   * @returns Content type, or null
+   * @return Content type, or null
+   * @see #getContentType(String) which is called with the file's name
    */
   public String getContentType(File file) 
   {
     return getContentType(file.getName());
-  } // getContentType()
+  }
 
-  /**
-   * Get content type of file.
-   * @param filename File to check
-   * @returns Content type, or null
+  /** get content type of file.
+   * Looks up the extension of the file (returns default content type if
+   * there is no extension) in successive registrys.
+   *
+   * @param filename name to check
+   * @return the first matching Content type, or the default if none matched
    */
-  public synchronized String getContentType(String filename) 
+  public String getContentType(String filename) 
   {
-    // Variables
     int index;
     String mimeType;
-    // MimeTypeEntry entry;
     String ext;
-    // Get Extension
+    //get the extension
     index = filename.lastIndexOf(".");
+    //if the filename has no extension then just return the def type
     if (index == -1) 
-    {
-      return defaultType;
-    }
-    ext = filename.substring(index + 1);
-    // Loop through each Mime Type File
-    for (index = 0; index < DB.length; index++) 
-    {
-      // Get Mime Type Entry
-      // entry = DB[index].getMimeTypeEntry(ext);
-      if (DB[index] == null) 
-      {
-	continue;
-      }
-      mimeType = DB[index].getMIMETypeString(ext);
-      // Return Mime Type
-      // if (entry != null)
-      //{
-      // return entry.getMimeType();
-      // }
-      if (mimeType != null) 
-      {
-	return mimeType;
-      }
-    } // for: index
     return defaultType;
-  } // getContentType()
+    //we must know the file extension
+    ext = filename.substring(index + 1);
+    //search each mime.type file
+    for(index=0; index<DB.length; index++) 
+    {
+      //if there is no list of types at this index skip it
+      if (DB[index] == null) 
+      continue;
+      //get the mime type from the list of types
+      mimeType = DB[index].get(ext);
+      if (mimeType != null) 
+      return mimeType.getBaseType();
+    }
+    return defaultType;
+  }
 
-  /**
-   * Programmically add a MIME Types entry.
+  /** programmically add MIME Types entries.
+   * In fact the MIME types specified here are put into the existing
+   * programmatic registry. These supplement that registry.
+   *
    * @param mime_types MIME Types formatted entry
    */
-  public synchronized void addMimeTypes(String mime_types) 
+  public void addMimeTypes(String mime_types) 
   {
-    DB[PROG].appendToRegistry(mime_types);
-  } // addMimeTypes()
-
-  /**
-   * Load MIME Type file.
-   * @param filename Name of file
-   * @returns MIME Type file
-   */
-  private MimeTypeFile loadFile(String filename) 
-  {
-    try 
+    try
     {
-      return new MimeTypeFile(filename);
-    } catch (Exception e) 
-    {
-      return null;
+      StringReader sr=new StringReader(mime_types);
+      DB[PROG].putAll(loadMimeRegistry(sr));
     }
-  } // loadFile()
+    catch(Exception e)
+    {
+      //don't bother with errors
+    }
+  }
 
-  /**
-   * Load MIME Type resource file.
-   * @param resource Name of resource
-   * @returns MIME Type file
+  /** load a MIME registry from the specified stream.
+   *
+   * @param in the stream to load from
+   * @return map of <code>MimeType</code> keyed by extension
    */
-  private MimeTypeFile loadResource(String resource) 
+  private Hashtable loadMimeRegistry(Reader in) 
   {
-    // Variables
-    InputStream stream;
-    // Get Resource Stream
-    stream = getClass().getClassLoader().getResourceAsStream(resource);
-    // Return MimeType File
     try 
     {
-      return new MimeTypeFile(stream);
+      Hashtable registry=new Hashtable();
+      //some states that we use in this mini-FSM
+      final int STARTLINE=0;
+      final int READTYPE=1;
+      final int READEXT=2;
+      //the state register
+      int state=STARTLINE;
+      //the mimetype register
+      MimeType mt=null;
+      //setup the tokenizer to parse a standard mime.types file
+      StreamTokenizer toker=new StreamTokenizer(in);
+      toker.setCommentChar('#');
+      toker.eolIsSignificant(true);
+      toker.setOrdinaryChar('/');
+      while(toker.hasMoreTokens())
+      {
+	switch(toker.nextToken())
+	{
+	  case StreamTokenizer.TT_EOF:
+	    return registry;
+	  case StreamTokenizer.TT_EOL:
+	    state=STARTLINE;
+	    continue;
+	  case StreamTokenizer.TT_WORD:
+	    switch(state)
+	    {
+	      case STARTLINE:
+		//create the mime type with the token we've just passed
+		mt=new MimeType(toker.sval);
+		//update the current state
+		state=READTYPE;
+		break;
+	      case READTYPE:
+		//the token must be an extension
+		registry.put(toker.sval,mt);
+		break;
+	    }
+	    continue;
+	  default:
+	    //should never get called
+	    break;
+	}
+      }
     }
     catch (Exception e) 
     {
-      return null;
+      //this is only here for debugging
+      e.printStackTrace();
     }
-  } // loadResource()
+    return null;
+  }
 
-} // MimetypesFileTypeMap
+  /** only for testing purposes.
+   */
+  public static void main(String[] argv)
+  {
+    MimetypesFileTypeMap fm=new MimetypesFileTypeMap();
+    String contentType=fm.getContentType("file.html");
+    System.out.println("the content type was: "+contentType);
+  }
+}
