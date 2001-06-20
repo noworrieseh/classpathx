cd82e8bfbe6b10484ff50d31aab4a4edd6464bcf
Ben Speakmon <bspeakmon@apache.org>
2001-06-20 22:50:09 +0000

    Changed header to standard LGPL boilerplate. Added self to authors list. Reformatted to use 2-space indents.
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@64 814015b8-e43b-4d26-bdac-3fba12e27a77

--- mail/source/gnu/mail/providers/smtp/SMTPOutputStream.java
+++ mail/source/gnu/mail/providers/smtp/SMTPOutputStream.java
@@ -1,16 +1,32 @@
-/********************************************************************
- * Copyright (c) Open Java Extensions, Andrew Selkirk  LGPL License *
- ********************************************************************/
+/*
+  GNU-Classpath Extensions: GNU Javamail - SMTP Service Provider
+  Copyright (C) 2001 Benjamin A. Speakmon
+
+  For more information on the classpathx please mail: classpathx-discuss@gnu.org
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
 
 package gnu.mail.providers.smtp;
 
-// Imports
 import java.io.IOException;
 import java.io.OutputStream;
 import gnu.mail.util.CRLFOutputStream;
 
-
-/** a stream to handle SMTP's concept of full stop.
+/** 
+ * A stream to handle SMTP's concept of full stop.
  * This stream wraps the Output stream that is generated in
  * SMTPTransport for data transmission of the message contents.
  * Since it is possible to include a '.' on a single line in a
@@ -18,11 +34,12 @@ import gnu.mail.util.CRLFOutputStream;
  * data stream), this stream protects the out bound data by
  * prepending another '.' according to the SMTP rfc.
  *
- * @author	Andrew Selkirk
- * @version	1.0
+ * @author      Andrew Selkirk
+ * @author      Ben Speakmon
+ * @version     1.0
  */
 public class SMTPOutputStream
-extends CRLFOutputStream
+  extends CRLFOutputStream
 {
 
   private int lastb;
@@ -35,7 +52,8 @@ extends CRLFOutputStream
     super(stream);
   }
 
-  /** write byte to SMTP output stream.
+  /** 
+   * Write a byte to the SMTP output stream.
    * If '.' is found according to the end-of-data guidelines,
    * then the byte is doubled.
    * @param b Byte to write
@@ -45,17 +63,18 @@ extends CRLFOutputStream
   {
     //check for '.' that was preceeded by eol character
     if (b == '.' && (lastb == '\r' || lastb == '\n')) 
-    {
-      out.write(new byte[]{46, 46}, 0, 2);
-      lastb = b;
-    }
+      {
+	out.write(new byte[]{46, 46}, 0, 2);
+	lastb = b;
+      }
     else 
-    {
-      super.write(b);
-    }
+      {
+	super.write(b);
+      }
   }
 
-  /** write byte array to SMTP output stream.
+  /** 
+   * Write a byte array to the SMTP output stream.
    * If '.' is found according to the end-of-data guidelines,
    * then the byte is doubled.
    * @param bytes Byte array to write
@@ -64,27 +83,27 @@ extends CRLFOutputStream
    * @throws IOException IO exception occurred
    */
   public void write(byte[] bytes, int offset, int length)
-  throws IOException 
+    throws IOException 
   {
     int index;
     int writeStart;
     //process Each Byte
     writeStart = offset;
     for (index = offset; index < length; index++) 
-    {
-      if (bytes[index] == '.') 
       {
-	// Output Previous bytes to CRLF layer
-	super.write(bytes, writeStart, index - writeStart);
-	// Write '.'
-	write('.');
-	// Track new Start
-	writeStart = index + 1;
+	if (bytes[index] == '.') 
+	  {
+	    // Output Previous bytes to CRLF layer
+	    super.write(bytes, writeStart, index - writeStart);
+	    // Write '.'
+	    write('.');
+	    // Track new Start
+	    writeStart = index + 1;
+	  }
       }
-    }
     // Check for Remaining bytes
     if (writeStart < length) 
-    super.write(bytes, writeStart, length - writeStart);
+      super.write(bytes, writeStart, length - writeStart);
   }
 
 }
