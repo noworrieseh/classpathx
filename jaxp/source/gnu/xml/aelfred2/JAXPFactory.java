f56edcb943145c913fbb80b83a9d18805b007091
David Brownell <db@DavidBrownell.com>
2001-06-20 20:39:39 +0000

    initial version
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@58 814015b8-e43b-4d26-bdac-3fba12e27a77

---
+++ jaxp/source/gnu/xml/aelfred2/JAXPFactory.java
@@ -0,0 +1,173 @@
+/*
+ * $Id: JAXPFactory.java,v 1.1 2001-06-20 20:39:39 db Exp $
+ * Copyright (C) 2001 David Brownell
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package gnu.xml.aelfred2;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import org.xml.sax.Parser;
+import org.xml.sax.XMLReader;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+
+/**
+ * Configurable factory to create an &AElig;lfred2 JAXP parser; required
+ * to bootstrap using JAXP.  You should use SAX2 directly where possible,
+ * rather than through JAXP, since that gives you better control.
+ * This class would normally be configured as a platform default factory.
+ *
+ * @author David Brownell
+ */
+public final class JAXPFactory extends SAXParserFactory
+{
+    private Hashtable	flags = new Hashtable ();
+
+    /**
+     * Constructs a factory which normally returns a non-validating
+     * parser.
+     */
+    JAXPFactory () { }
+
+    public SAXParser newSAXParser ()
+    throws ParserConfigurationException, SAXException
+    {
+	JaxpParser	jaxp = new JaxpParser ();
+	Enumeration	e = flags.keys ();
+	XMLReader	parser = jaxp.getXMLReader ();
+
+	parser.setFeature (
+		SAXDriver.FEATURE + "namespaces",
+		isNamespaceAware ());
+	parser.setFeature (
+		SAXDriver.FEATURE + "validating",
+		isValidating ());
+	// that makes SAX2 feature flags trump JAXP
+
+	while (e.hasMoreElements ()) {
+	    String	uri = (String) e.nextElement ();
+	    Boolean	value = (Boolean) flags.get (uri);
+	    parser.setFeature (uri, value.booleanValue ());
+	}
+
+	return jaxp;
+    }
+
+    // yes, this "feature transfer" mechanism doesn't play well
+
+    public void setFeature (String name, boolean value) 
+    throws
+	ParserConfigurationException,
+	SAXNotRecognizedException,
+	SAXNotSupportedException
+    {
+	try {
+	    // force "early" detection of errors where possible
+	    // (flags can't necessarily be set before parsing)
+	    new JaxpParser ().getXMLReader ().setFeature (name, value);
+
+	    flags.put (name, new Boolean (value));
+	} catch (SAXNotRecognizedException e) {
+	    throw new SAXNotRecognizedException (name);
+	} catch (SAXNotSupportedException e) {
+	    throw new SAXNotSupportedException (name);
+	} catch (Exception e) {
+	    throw new ParserConfigurationException (
+		  e.getClass ().getName ()
+		+ ": "
+		+ e.getMessage ());
+	}
+    }
+
+    public boolean getFeature (String name) 
+    throws
+	ParserConfigurationException,
+	SAXNotRecognizedException,
+	SAXNotSupportedException
+    {
+	Boolean	value = (Boolean) flags.get (name);
+	
+	if (value == null)
+	    return value.booleanValue ();
+	else
+	    try {
+		return new JaxpParser ().getXMLReader ().getFeature (name);
+	    } catch (SAXNotRecognizedException e) {
+		throw new SAXNotRecognizedException (name);
+	    } catch (SAXNotSupportedException e) {
+		throw new SAXNotSupportedException (name);
+	    } catch (SAXException e) {
+		throw new ParserConfigurationException (
+		      e.getClass ().getName ()
+		    + ": "
+		    + e.getMessage ());
+	    }
+    }
+
+    private static class JaxpParser extends SAXParser
+    {
+
+// FIXME:  Want a new AElfred2 XMLReader class,
+// with settable validation flag
+
+	private SAXDriver	ae2 = new SAXDriver ();
+
+	JaxpParser () { }
+
+	public void setProperty (String id, Object value) 
+	throws SAXNotRecognizedException, SAXNotSupportedException
+	    { ae2.setProperty (id, value); }
+
+	public Object getProperty (String id) 
+	throws SAXNotRecognizedException, SAXNotSupportedException
+	    { return ae2.getProperty (id); }
+
+	public Parser getParser ()
+	throws SAXException
+	    { return ae2; }
+
+	public XMLReader getXMLReader ()
+	throws SAXException
+	    { return ae2; }
+
+	public boolean isNamespaceAware ()
+	{
+	    try {
+		return ae2.getFeature (SAXDriver.FEATURE + "namespaces");
+	    } catch (Exception e) {
+		throw new Error ();
+	    }
+	}
+
+	public boolean isValidating ()
+	{
+	    try {
+		return ae2.getFeature (SAXDriver.FEATURE + "validation");
+	    } catch (Exception e) {
+		throw new Error ();
+	    }
+	}
+    }
+}
