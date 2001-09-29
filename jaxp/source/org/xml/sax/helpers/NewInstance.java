b9154f7e9de064d777668c79f8257b76021cfb95
David Brownell <db@DavidBrownell.com>
2001-09-29 07:25:29 +0000

    added from sax@sourceforge
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@180 814015b8-e43b-4d26-bdac-3fba12e27a77

---
+++ jaxp/source/org/xml/sax/helpers/NewInstance.java
@@ -0,0 +1,96 @@
+// NewInstance.java - create a new instance of a class by name.
+// Written by Edwin Goei, edwingo@apache.org
+// and by David Brownell, dbrownell@users.sourceforge.net
+// NO WARRANTY!  This class is in the Public Domain.
+
+// $Id: NewInstance.java,v 1.1 2001-09-29 07:25:29 db Exp $
+
+package org.xml.sax.helpers;
+
+/**
+ * Create a new instance of a class by name.
+ *
+ * <blockquote>
+ * <em>This module, both source code and documentation, is in the
+ * Public Domain, and comes with <strong>NO WARRANTY</strong>.</em>
+ * See <a href='http://sax.sourceforge.net'>http://sax.sourceforge.net</a>
+ * for further information.
+ * </blockquote>
+ *
+ * <p>This class contains a static method for creating an instance of a
+ * class from an explicit class name.  It tries to use the thread's context
+ * ClassLoader if possible and falls back to using
+ * Class.forName(String).</p>
+ *
+ * <p>This code is designed to run on JDK version 1.1 and later including
+ * JVMs that perform early linking like the Microsoft JVM in IE 5.  Note
+ * however that it must be compiled on a JDK version 1.2 or later system
+ * since it calls Thread#getContextClassLoader().  The code also runs both
+ * as part of an unbundled jar file and when bundled as part of the
+ * JDK.</p>
+ *
+ * @author Edwin Goei, David Brownell
+ * @version $Id: NewInstance.java,v 1.1 2001-09-29 07:25:29 db Exp $
+ */
+class NewInstance {
+
+    /**
+     * Creates a new instance of the specified class name
+     *
+     * Package private so this code is not exposed at the API level.
+     */
+    static Object newInstance (ClassLoader classLoader, String className)
+        throws ClassNotFoundException, IllegalAccessException,
+            InstantiationException
+    {
+        Class driverClass;
+        if (classLoader == null) {
+            driverClass = Class.forName(className);
+        } else {
+            driverClass = classLoader.loadClass(className);
+        }
+        return driverClass.newInstance();
+    }
+
+    static ClassLoader getClassLoader ()
+    {
+        ClassLoader classLoader;
+        try {
+            // Construct the name of the concrete class to instantiate
+            Class clazz = Class.forName(NewInstance.class.getName()
+                                        + "$ClassLoaderFinderConcrete");
+            ClassLoaderFinder clf = (ClassLoaderFinder) clazz.newInstance();
+            return clf.getContextClassLoader();
+
+	// the inner classes (below) were corrupted -- "can't happen"
+        } catch (InstantiationException e) {
+            throw new UnknownError (e.getMessage ());
+        } catch (IllegalAccessException e) {
+            throw new UnknownError (e.getMessage ());
+
+        } catch (LinkageError e) {
+            // Assume that we are running JDK 1.1, use the current ClassLoader
+        } catch (ClassNotFoundException e) {
+            // This case should not normally happen.  MS IE can throw this
+            // instead of a LinkageError the second time Class.forName() is
+            // called so assume that we are running JDK 1.1 and use the
+            // current ClassLoader
+        }
+	return NewInstance.class.getClassLoader();
+    }
+
+    /*
+     * The following nested classes allow getContextClassLoader() to be
+     * called only on JDK 1.2 and yet run in older JDK 1.1 JVMs
+     */
+
+    private static abstract class ClassLoaderFinder {
+        abstract ClassLoader getContextClassLoader();
+    }
+
+    static class ClassLoaderFinderConcrete extends ClassLoaderFinder {
+        ClassLoader getContextClassLoader() {
+            return Thread.currentThread().getContextClassLoader();
+        }
+    }
+}
