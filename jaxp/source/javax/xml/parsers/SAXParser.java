fdb83fadba658ab3cc7386e8c7fb1e85c1136b31
David Brownell <db@DavidBrownell.com>
2001-11-02 21:39:57 +0000

    license update; don't discard some URIs
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@232 814015b8-e43b-4d26-bdac-3fba12e27a77

--- jaxp/source/javax/xml/parsers/SAXParser.java
+++ jaxp/source/javax/xml/parsers/SAXParser.java
@@ -1,23 +1,30 @@
 /*
-  GNU-Classpath Extensions:	jaxp
-  Copyright (C) 2001 Andrew Selkirk
-
-  For more information on the classpathx please mail: classpathx-discuss@gnu.org
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*/
+ * $Id: SAXParser.java,v 1.3 2001-11-02 21:39:57 db Exp $
+ * Copyright (C) 2001 Andrew Selkirk
+ * Copyright (C) 2001 David Brownell
+ * 
+ * This file is part of GNU JAXP, a library.
+ *
+ * GNU JAXP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * GNU JAXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License. 
+ */
 
 package javax.xml.parsers;
 
@@ -29,7 +36,7 @@ import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
 /**
- * SAXParser
+ * Wraps a SAX2 (or SAX1) parser.
  * @author	Andrew Selkirk
  * @version	1.0
  */
@@ -47,10 +54,10 @@ public abstract class SAXParser {
 	// Methods ----------------------------------------------------
 	//-------------------------------------------------------------
 
-	public abstract void setProperty(String value1, Object value2) 
+	public abstract void setProperty (String id, Object value) 
 		throws SAXNotRecognizedException, SAXNotSupportedException;
 
-	public abstract Object getProperty(String value) 
+	public abstract Object getProperty (String id) 
 		throws SAXNotRecognizedException, SAXNotSupportedException;
 
 	public void parse(InputStream stream, HandlerBase handler) 
@@ -58,8 +65,11 @@ public abstract class SAXParser {
 		parse(new InputSource(stream), handler);
 	} // parse()
 
-	public void parse(InputStream stream, HandlerBase handler, String systemID)
-		throws SAXException, IOException {
+	public void parse (
+		InputStream stream,
+		HandlerBase handler,
+		String systemID
+	) throws SAXException, IOException {
 
 		// Variables
 		InputSource	source;
@@ -77,8 +87,11 @@ public abstract class SAXParser {
 		parse(new InputSource(stream), def);
 	} // parse()
 
-	public void parse(InputStream stream, DefaultHandler def, String systemID) 
-		throws SAXException, IOException {
+	public void parse (
+		InputStream stream,
+		DefaultHandler def,
+		String systemID
+	) throws SAXException, IOException {
 
 		// Variables
 		InputSource	source;
@@ -93,21 +106,23 @@ public abstract class SAXParser {
 
 	public void parse(String uri, HandlerBase handler) 
 		throws SAXException, IOException {
-		parse(new InputSource(new URL(uri).openStream()), handler);
+		parse(new InputSource(uri), handler);
 	} // parse()
 
 	public void parse(String uri, DefaultHandler def) 
 		throws SAXException, IOException {
-		parse(new InputSource(new URL(uri).openStream()), def);
+		parse(new InputSource(uri), def);
 	} // parse()
 
 	public void parse(File file, HandlerBase handler) 
 		throws SAXException, IOException {
+// FIXME:  never discard the uri!  fileToURI()
 		parse(new InputSource(new FileInputStream(file)), handler);
 	} // parse()
 
 	public void parse(File file, DefaultHandler def) 
 		throws SAXException, IOException {
+// FIXME:  never discard the uri!  fileToURI()
 		parse(new InputSource(new FileInputStream(file)), def);
 	} // parse()
 
@@ -124,7 +139,7 @@ public abstract class SAXParser {
 		parser.setEntityResolver(handler);
 		parser.setErrorHandler(handler);
 
-		// Parse stream
+		// Parse
 		parser.parse(source);
 
 	} // parse()
@@ -142,7 +157,10 @@ public abstract class SAXParser {
 		reader.setEntityResolver(def);
 		reader.setErrorHandler(def);
 
-		// Parse stream
+		// FIXME: if "def" implements lexical or decl handler,
+		// set those properties ...
+
+		// Parse
 		reader.parse(source);
 
 	} // parse()
@@ -154,9 +172,4 @@ public abstract class SAXParser {
 	public abstract boolean isNamespaceAware();
 
 	public abstract boolean isValidating();
-
-
-} // SAXParser
-
-
-
+}
