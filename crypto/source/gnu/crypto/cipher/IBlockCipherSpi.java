e8095629cf436357e6fc63763ee83c15b18cba6b
Raif S. Naffah <go@naffah-raif.name>
2001-11-21 19:43:23 +0000

    + defined the mandatory methods to be implemented by implementation subclasses   of the BaseXxx class as a package-private interface named yyySpi, where   yyy is the 'public' version of the interface; eg. IBlockCipher -> IBlockCipherSpi.   adds safety when implementing new subclasses.  still need to do the same   for the other packages.
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@321 814015b8-e43b-4d26-bdac-3fba12e27a77

---
+++ crypto/source/gnu/crypto/cipher/IBlockCipherSpi.java
@@ -0,0 +1,103 @@
+package gnu.crypto.cipher;
+
+// ----------------------------------------------------------------------------
+// $Id: IBlockCipherSpi.java,v 1.1 2001-11-21 19:43:23 raif Exp $
+//
+// Copyright (C) 2001 Free Software Foundation, Inc.
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Library General Public License as published by the Free
+// Software Foundation; either version 2 of the License or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with this program; see the file COPYING. If not, write to the
+//
+//    Free Software Foundation Inc.,
+//    59 Temple Place - Suite 330,
+//    Boston, MA 02111-1307
+//    USA
+// ----------------------------------------------------------------------------
+
+import java.security.InvalidKeyException;
+import java.util.Iterator;
+
+/**
+ * Package-private interface exposing mandatory methods to be implemented by
+ * concrete {@link gnu.crypto.cipher.BaseCipher} sub-classes.
+ *
+ * @version $Revision: 1.1 $
+ */
+interface IBlockCipherSpi extends Cloneable {
+
+   // Constants
+   // -------------------------------------------------------------------------
+
+   // Methods
+   // -------------------------------------------------------------------------
+
+   /**
+    * Returns an {@link java.util.Iterator} over the supported block sizes.
+    * Each element returned by this object is a {@link java.lang.Integer}.<p>
+    *
+    * @return an <tt>Iterator</tt> over the supported block sizes.
+    */
+   Iterator blockSizes();
+
+   /**
+    * Returns an {@link java.util.Iterator} over the supported key sizes. Each
+    * element returned by this object is a {@link java.lang.Integer}.<p>
+    *
+    * @return an <tt>Iterator</tt> over the supported key sizes.
+    */
+   Iterator keySizes();
+
+   /**
+    * Expands a user-supplied key material into a session key for a designated
+    * <i>block size</i>.<p>
+    *
+    * @param k the user-supplied key material.
+    * @param bs the desired block size in bytes.
+    * @return an Object encapsulating the session key.
+    * @exception IllegalArgumentException if the block size is invalid.
+    * @exception InvalidKeyException if the key data is invalid.
+    */
+   Object makeKey(byte[]k, int bs) throws InvalidKeyException;
+
+   /**
+    * Encrypts exactly one block of plaintext.<p>
+    *
+    * @param in the plaintext.
+    * @param inOffset index of <i>in</i> from which to start considering data.
+    * @param out the ciphertext.
+    * @param outOffset index of <i>out</i> from which to store the result.
+    * @param k the session key to use.
+    * @param bs the block size to use.
+    * @exception IllegalArgumentException if the block size is invalid.
+    * @exception ArrayIndexOutOfBoundsException if there is not enough room in
+    * either the plaintext or ciphertext buffers.
+    */
+   void
+   encrypt(byte[] in, int inOffset, byte[] out, int outOffset, Object k, int bs);
+
+   /**
+    * Decrypts exactly one block of ciphertext.<p>
+    *
+    * @param in the ciphertext.
+    * @param inOffset index of <i>in</i> from which to start considering data.
+    * @param out the plaintext.
+    * @param outOffset index of <i>out</i> from which to store the result.
+    * @param k the session key to use.
+    * @param bs the block size to use.
+    * @exception IllegalArgumentException if the block size is invalid.
+    * @exception ArrayIndexOutOfBoundsException if there is not enough room in
+    * either the plaintext or ciphertext buffers.
+    */
+   void
+   decrypt(byte[] in, int inOffset, byte[] out, int outOffset, Object k, int bs);
+}
