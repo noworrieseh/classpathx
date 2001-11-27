0b70ae758d7b3aedffbde7992b0e80b38c5c13d9
Andrew Selkirk <alselkirk@gmail.com>
2001-11-27 05:43:53 +0000

    license and doc updates
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@336 814015b8-e43b-4d26-bdac-3fba12e27a77

--- infobus/source/javax/infobus/InfoBusItemRequestedEvent.java
+++ infobus/source/javax/infobus/InfoBusItemRequestedEvent.java
@@ -1,7 +1,18 @@
-/********************************************************************
- * Copyright (c) Open Java Extensions, Andrew Selkirk  LGPL License *
- ********************************************************************/
-
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package javax.infobus;
 
 // Imports
@@ -9,6 +20,8 @@ import java.awt.datatransfer.DataFlavor;
 
 /**
  * InfoBus Item Requested Event.
+ * @author Andrew Selkirk
+ * @version $Revision: 1.2 $
  */
 public final	class	InfoBusItemRequestedEvent
 		extends	InfoBusEvent {
--- infobus/source/javax/infobus/InfoBusMember.java
+++ infobus/source/javax/infobus/InfoBusMember.java
@@ -1,7 +1,18 @@
-/********************************************************************
- * Copyright (c) Open Java Extensions, Andrew Selkirk  LGPL License *
- ********************************************************************/
-
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package javax.infobus;
 
 // Imports
@@ -11,8 +22,10 @@ import java.beans.PropertyChangeListener;
 
 /**
  * InfoBus Member interface.
+ * @author Andrew Selkirk
+ * @version $Revision: 1.2 $
  */
-public abstract interface InfoBusMember {
+public interface InfoBusMember {
 
 	//-------------------------------------------------------------
 	// Interface: InfoBusMember -----------------------------------
