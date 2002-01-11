2281b85544535aa556b600feb14680ce5538170b
Raif S. Naffah <go@naffah-raif.name>
2002-01-11 21:42:38 +0000

    + code refactoring: added isProbablePrime() method.
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@409 814015b8-e43b-4d26-bdac-3fba12e27a77

--- crypto/source/gnu/crypto/util/Prime.java
+++ crypto/source/gnu/crypto/util/Prime.java
@@ -1,7 +1,7 @@
 package gnu.crypto.util;
 
 // ----------------------------------------------------------------------------
-// $Id: Prime.java,v 1.1 2001-12-30 15:56:08 raif Exp $
+// $Id: Prime.java,v 1.2 2002-01-11 21:42:38 raif Exp $
 //
 // Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 //
@@ -30,17 +30,13 @@ package gnu.crypto.util;
 // be covered by the GNU General Public License.
 // ----------------------------------------------------------------------------
 
-import gnu.crypto.prng.IRandom;
-import gnu.crypto.prng.LimitReachedException;
-import gnu.crypto.prng.MDGenerator;
 import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.util.HashMap;
 
 /**
  * A collection of prime number related utility methods used in this library.
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class Prime {
 
@@ -49,15 +45,23 @@ public class Prime {
 
    private static final String NAME = "prime";
    private static final boolean DEBUG = true;
-   private static final int debuglevel = 5;
+   private static final int debuglevel = 1;
    private static final PrintWriter err = new PrintWriter(System.out, true);
    private static void debug(String s) {
       err.println(">>> "+NAME+": "+s);
    }
 
+   /**
+    * By default do (when <code>true</code>) or skip (when <code>false</code>)
+    * The Miller-Rabin probabilistic primality test.<p>
+    *
+    * @see #isProbablePrime
+    */
+   private static final boolean DO_MILLER_RABIN = false;
+
    // Constants and variables
    // -------------------------------------------------------------------------
-   
+
    private static final BigInteger ZERO = BigInteger.ZERO;
    private static final BigInteger ONE = BigInteger.ONE;
    private static final BigInteger TWO = BigInteger.valueOf(2L);
@@ -109,17 +113,6 @@ public class Prime {
       }
    }
 
-   /** SHA-based random number generator. */
-   private static final IRandom prng;
-   static {
-      prng = new MDGenerator();
-      try {
-         prng.init(new HashMap()); // default is to use SHA
-      } catch (Exception x) {
-         throw new ExceptionInInitializerError(x);
-      }
-   }
-
    // Constructor(s)
    // -------------------------------------------------------------------------
 
@@ -130,7 +123,7 @@ public class Prime {
 
    // Class methods
    // -------------------------------------------------------------------------
-   
+
    /**
     * Trial division for the first 1000 small primes.<p>
     *
@@ -158,7 +151,7 @@ public class Prime {
       }
       return false;
    }
-   
+
    /**
     * Java port of Colin Plumb primality test (Euler Criterion) implementation
     * for a base of 2 --from bnlib-1.1 release, function primeTest() in prime.c.
@@ -168,43 +161,43 @@ public class Prime {
     * beyond all reasonable doubt, and everything else is just gravy, but it
     * gives people warm fuzzies to do it.<p>
     *
-    * This starts with verifying Euler's criterion for a base of 2. This is the 
+    * This starts with verifying Euler's criterion for a base of 2. This is the
     * fastest pseudoprimality test that I know of, saving a modular squaring
-    * over a Fermat test, as well as being stronger. 7/8 of the time, it's as 
+    * over a Fermat test, as well as being stronger. 7/8 of the time, it's as
     * strong as a strong pseudoprimality test, too. (The exception being when
-    * <code>bn == 1 mod 8</code> and <code>2</code> is a quartic residue, i.e. 
-    * <code>bn</code> is of the form <code>a^2 + (8*b)^2</code>.) The precise 
-    * series of tricks used here is not documented anywhere, so here's an 
-    * explanation. Euler's criterion states that if <code>p</code> is prime 
+    * <code>bn == 1 mod 8</code> and <code>2</code> is a quartic residue, i.e.
+    * <code>bn</code> is of the form <code>a^2 + (8*b)^2</code>.) The precise
+    * series of tricks used here is not documented anywhere, so here's an
+    * explanation. Euler's criterion states that if <code>p</code> is prime
     * then <code>a^((p-1)/2)</code> is congruent to <code>Jacobi(a,p)</code>,
     * modulo <code>p</code>. <code>Jacobi(a, p)</code> is a function which is
     * <code>+1</code> if a is a square modulo <code>p</code>, and <code>-1</code>
-    * if it is not. For <code>a = 2</code>, this is particularly simple. It's 
-    * <code>+1</code> if <code>p == +/-1 (mod 8)</code>, and <code>-1</code> if 
-    * <code>m == +/-3 (mod 8)</code>. If <code>p == 3 (mod 4)</code>, then all 
-    * a strong test does is compute <code>2^((p-1)/2)</code>. and see if it's 
+    * if it is not. For <code>a = 2</code>, this is particularly simple. It's
+    * <code>+1</code> if <code>p == +/-1 (mod 8)</code>, and <code>-1</code> if
+    * <code>m == +/-3 (mod 8)</code>. If <code>p == 3 (mod 4)</code>, then all
+    * a strong test does is compute <code>2^((p-1)/2)</code>. and see if it's
     * <code>+1</code> or <code>-1</code>. (Euler's criterion says <i>which</i>
     * it should be.) If <code>p == 5 (mod 8)</code>, then <code>2^((p-1)/2)</code>
-    * is <code>-1</code>, so the initial step in a strong test, looking at 
-    * <code>2^((p-1)/4)</code>, is wasted --you're not going to find a 
-    * <code>+/-1</code> before then if it <b>is</b> prime, and it shouldn't 
+    * is <code>-1</code>, so the initial step in a strong test, looking at
+    * <code>2^((p-1)/4)</code>, is wasted --you're not going to find a
+    * <code>+/-1</code> before then if it <b>is</b> prime, and it shouldn't
     * have either of those values if it isn't. So don't bother.<p>
     *
     * The remaining case is <code>p == 1 (mod 8)</code>. In this case, we
-    * expect <code>2^((p-1)/2) == 1 (mod p)</code>, so we expect that the 
+    * expect <code>2^((p-1)/2) == 1 (mod p)</code>, so we expect that the
     * square root of this, <code>2^((p-1)/4)</code>, will be <code>+/-1 (mod p)
-    * </code>. Evaluating this saves us a modular squaring 1/4 of the time. If 
+    * </code>. Evaluating this saves us a modular squaring 1/4 of the time. If
     * it's <code>-1</code>, a strong pseudoprimality test would call <code>p</code>
-    * prime as well. Only if the result is <code>+1</code>, indicating that 
-    * <code>2</code> is not only a quadratic residue, but a quartic one as well, 
+    * prime as well. Only if the result is <code>+1</code>, indicating that
+    * <code>2</code> is not only a quadratic residue, but a quartic one as well,
     * does a strong pseudoprimality test verify more things than this test does.
     * Good enough.<p>
     *
     * We could back that down another step, looking at <code>2^((p-1)/8)</code>
-    * if there was a cheap way to determine if <code>2</code> were expected to 
-    * be a quartic residue or not. Dirichlet proved that <code>2</code> is a 
+    * if there was a cheap way to determine if <code>2</code> were expected to
+    * be a quartic residue or not. Dirichlet proved that <code>2</code> is a
     * quadratic residue iff <code>p</code> is of the form <code>a^2 + (8*b^2)</code>.
-    * All primes <code>== 1 (mod 4)</code> can be expressed as <code>a^2 + 
+    * All primes <code>== 1 (mod 4)</code> can be expressed as <code>a^2 +
     * (2*b)^2</code>, but I see no cheap way to evaluate this condition."<p>
     *
     * @param w the number to test.
@@ -308,9 +301,9 @@ public class Prime {
       }
       return true;
    }
-   
+
    /**
-    * Checks Fermat's Little Theorem for base 2; i.e. <code>2**(w-1) == 1 
+    * Checks Fermat's Little Theorem for base 2; i.e. <code>2**(w-1) == 1
     * (mod w)</code>.<p>
     *
     * @param w the number to test.
@@ -328,13 +321,13 @@ public class Prime {
       }
       return result;
    }
-   
+
    /**
     * Applies the Miller-Rabin strong probabilistic primality test.<p>
     *
-    * The HAC (Handbook of Applied Cryptography), Alfred Menezes & al. Note 
-    * 4.57 states that for <code>q</code>, <code>n=18</code> is enough while 
-    * for <code>p</code>, <code>n=6</code> (512 bits) or <code>n=3</cdoe> (1024 
+    * The HAC (Handbook of Applied Cryptography), Alfred Menezes & al. Note
+    * 4.57 states that for <code>q</code>, <code>n=18</code> is enough while
+    * for <code>p</code>, <code>n=6</code> (512 bits) or <code>n=3</cdoe> (1024
     * bits) are enough to yield <i>robust</i> primality tests. The values used
     * are from table 4.4 given in Note 4.49.<p>
     *
@@ -369,10 +362,7 @@ public class Prime {
       step3: for (int i = 0; i < n; i++) {
          // 3. Generate a random integer b in the range 1 < b < w.
          do {
-            try {
-               prng.nextBytes(kb, 0, wlen);
-            } catch (LimitReachedException ignored) {
-            }
+            PRNG.nextBytes(kb);
             b = new BigInteger(1, kb);
          } while (ONE.compareTo(b) < 0 && b.compareTo(w) < 0);
          // 4. Set j = 0 and z = b**m mod w
@@ -397,6 +387,104 @@ public class Prime {
       if (DEBUG && debuglevel > 4) {
          debug(w.toString(16)+" passes Miller-Rabin test...");
       }
+      return true;
+   }
+
+   /**
+    * Calls the method with same name and two arguments using the pre-configured
+    * value for <code>DO_MILLER_RABIN</code>.
+    *
+    * @param w the integer to test.
+    * @return <code>true</code> iff the designated number has no small prime
+    * divisor passes the Euler criterion, and optionally a Miller-Rabin test.
+    * @see #DO_MILLER_RABIN
+    */
+   public static boolean isProbablePrime(BigInteger w) {
+      return isProbablePrime(w, DO_MILLER_RABIN);
+   }
+
+   /**
+    * This implementation does not rely solely on the Miller-Rabin strong
+    * probabilistic primality test to claim the primality of the designated
+    * number. It instead, tries dividing the designated number by the first 1000
+    * small primes, and if no divisor was found, invokes a port of Colin Plumb's
+    * implementation of the Euler Criterion, with the option --passed as one of
+    * its arguments-- to follow with the Miller-Rabin test.
+    *
+    * @param w the integer to test.
+    * @param doMillerRabin if <code>true</code> and the designated integer was
+    * already found to be a probable prime, then also do a Miller-Rabin test.
+    * @return <code>true</code> iff the designated number has no small prime
+    * divisor passes the Euler criterion, and optionally a Miller-Rabin test.
+    */
+   public static boolean isProbablePrime(BigInteger w, boolean doMillerRabin) {
+      // eliminate trivial cases when w == 0 or 1
+      if (w.equals(ZERO)) {
+         return false;
+      } else if (w.equals(ONE)) {
+         return true;
+      }
+
+      // trial division with first 1000 primes
+      if (hasSmallPrimeDivisor(w)) {
+         if (DEBUG && debuglevel > 4) {
+            debug(w.toString(16)+" has a small prime divisor. Rejected...");
+         }
+         return false;
+      }
+
+      // the following code is commented out since it's a special case (base 2)
+      // of Euler's criterion.
+//      if (passFermatLittleTheorem(w)) {
+//         if (DEBUG && debuglevel > 4) {
+//            debug(w.toString(16)+" passes Fermat's Little Theorem...");
+//         }
+//      } else {
+//         if (DEBUG && debuglevel > 4) {
+//            debug(w.toString(16)+" fails Fermat's Little Theorem. Rejected...");
+//         }
+//         return false;
+//      }
+
+      if (passEulerCriterion(w)) {
+         if (DEBUG && debuglevel > 4) {
+            debug(w.toString(16)+" passes Euler criterion...");
+         }
+      } else {
+         if (DEBUG && debuglevel > 4) {
+            debug(w.toString(16)+" fails Euler criterion. Rejected...");
+         }
+         return false;
+      }
+
+      // Miller-Rabin probabilistic primality test.
+      if (doMillerRabin) {
+         if (passMillerRabin(w)) {
+            if (DEBUG && debuglevel > 4) {
+               debug(w.toString(16)+" passes Miller-Rabin PPT...");
+            }
+         } else {
+            if (DEBUG && debuglevel > 4) {
+               debug(w.toString(16)+" fails Miller-Rabin PPT. Rejected...");
+            }
+            return false;
+         }
+      }
+
+      if (DEBUG && debuglevel > 4) {
+         debug(w.toString(16)+" is probable prime. Accepted...");
+      }
+
+      // now compare to JDK primality test
+      if (DEBUG && debuglevel > 0 && !w.isProbablePrime(100)) {
+         System.err.println("The gnu.crypto library and the JDK disagree on "
+            +"whether 0x"+w.toString(16)+" is a probable prime or not.");
+         System.err.println("While this library claims it is, the JDK claims"
+            +" the opposite.");
+         System.err.println("Please contact the maintainer of this library, "
+            +"and provide this message for further investigation. TIA");
+      }
+
       return true;
    }
 
