f57fc2ed21a3bb570ea0b52e66cfe59eb9ae760f
Nic Ferrier <nferrier@gnu.org>
2002-03-05 13:05:14 +0000

    added missing JSP classes.
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@451 814015b8-e43b-4d26-bdac-3fba12e27a77

---
+++ servletapi/source/javax/servlet/jsp/JspTagException.java
@@ -0,0 +1,48 @@
+/*
+ * JspTagException.java.
+ * 
+ * Copyright (c) 2002 by Free Software Foundation, Inc.
+ * Written by Nic Ferrier <nferrier@tapsellferrier.co.uk>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation, version 2. (see COPYING.LIB)
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation  
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package javax.servlet.jsp;
+
+
+/**
+ * A JSP specific exception.
+ *
+ * @author Nic Ferrier <nferrier@tapsellferrier.co.uk>
+ */
+public class JspTagException extends JspException
+{
+
+  /** Make an exception with a default error message.
+   */
+  public JspTagException ()
+  {
+    super("jsp tag problem");
+  }
+
+
+
+  /** Make an exception with the specified error message.
+   */
+  public JspTagException (String message)
+  {
+    super(message);
+  }
+  
+}
