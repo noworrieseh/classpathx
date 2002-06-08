67c3dbb89ba040e102c72b450c8cb3b5769b904b
Raif S. Naffah <go@naffah-raif.name>
2002-06-08 05:14:20 +0000

    + changed the property name gnu.crypto.operational.state to    gnu.crypto.mode.state. + updated the copyright year. + aesthetics.
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@504 814015b8-e43b-4d26-bdac-3fba12e27a77

--- crypto/source/gnu/crypto/mode/IMode.java
+++ crypto/source/gnu/crypto/mode/IMode.java
@@ -1,9 +1,9 @@
 package gnu.crypto.mode;
 
 // ----------------------------------------------------------------------------
-// $Id: IMode.java,v 1.2 2001-12-04 12:56:08 raif Exp $
+// $Id: IMode.java,v 1.3 2002-06-08 05:14:20 raif Exp $
 //
-// Copyright (C) 2001 Free Software Foundation, Inc.
+// Copyright (C) 2001-2002, Free Software Foundation, Inc.
 //
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the Free
@@ -33,44 +33,51 @@ package gnu.crypto.mode;
 import gnu.crypto.cipher.IBlockCipher;
 
 /**
- * The basic visible methods of any block cipher mode.<p>
+ * <p>The basic visible methods of any block cipher mode.</p>
  *
- * Block ciphers encrypt plaintext in fixed size n-bit blocks. For messages
+ * <p>Block ciphers encrypt plaintext in fixed size n-bit blocks. For messages
  * larger than n bits, the simplest approach is to segment the message into
  * n-bit blocks and process (encrypt and/or decrypt) each one separately
  * (Electronic Codebook or ECB mode). But this approach has disadvantages in
  * most applications. The block cipher modes of operations are one way of
- * working around those disadvantages.<p>
+ * working around those disadvantages.</p>
  *
- * A <i>Mode</i> always employs an underlying block cipher for processing its
+ * <p>A <i>Mode</i> always employs an underlying block cipher for processing its
  * input. For all intents and purposes, a <i>Mode</i> appears to behave as any
- * other block cipher with the following differences:
+ * other block cipher with the following differences:</p>
+ *
  * <ul>
  *    <li>Depending on the specifications of the mode, the block size may be
  *    different that that of the underlying cipher.</li>
+ *
  *    <li>While some modes of operations allow operations on block sizes that
  *    can be 1-bit long, this library will only deal with sizes that are
  *    multiple of 8 bits. This is because the <tt>byte</tt> is the smallest,
  *    easy to handle, primitive type in Java.</li>
+ *
  *    <li>Some modes need an <i>Initialisation Vector</i> (IV) to be properly
  *    initialised.</li>
  * </ul>
  *
- * Possible additional initialisation values for an instance of that type are:
+ * <p>Possible additional initialisation values for an instance of that type
+ * are:</p>
+ *
  * <ul>
  *    <li>The block size in which to operate this mode instance. This
  *    value is <b>optional</b>, if unspecified, the underlying block cipher's
- *    configured block size shall be used.
+ *    configured block size shall be used.</li>
+ *
  *    <li>Whether this mode will be used for encryption or decryption. This
  *    value is <b>mandatory</b> and should be included in the initialisation
  *    parameters. If it isn't, a {@link java.lang.IllegalStateException} will
- *    be thrown if any method, other than <tt>reset()</tt> is invoked on the
+ *    be thrown if any method, other than <code>reset()</code> is invoked on the
  *    instance.</li>
+ *
  *    <li>The byte array containing the <i>initialisation vector</i>, if
  *    required by this type of mode.</li>
  * </ul>
  *
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public interface IMode extends IBlockCipher {
 
@@ -78,49 +85,52 @@ public interface IMode extends IBlockCipher {
    // -------------------------------------------------------------------------
 
    /**
-    * Property name of the state in which to operate this mode. The value
-    * associated to this property name is taken to be an instance of
-    * {@link java.lang.Integer} which value is either <tt>ENCRYPTION</tt> or
-    * <tt>DECRYPTION</tt>.
+    * <p>Property name of the state in which to operate this mode. The value
+    * associated to this property name is taken to be an {@link Integer} which
+    * value is either <code>ENCRYPTION</code> or <code>DECRYPTION</code>.</p>
     */
-   String STATE = "gnu.crypto.operational.state";
+   String STATE = "gnu.crypto.mode.state";
 
    /**
-    * Property name of the block size in which to operate this mode. The
-    * value associated with this property name is taken to be a
-    * {@link java.lang.Integer}. If it is not specified, the value of the
-    * block size of the underlying block cipher, used to construct the mode
-    * instance, shall be used.
+    * <p>Property name of the block size in which to operate this mode. The
+    * value associated with this property name is taken to be an {@link Integer}.
+    * If it is not specified, the value of the block size of the underlying
+    * block cipher, used to construct the mode instance, shall be used.</p>
     */
    String MODE_BLOCK_SIZE = "gnu.crypto.mode.block.size";
 
    /**
-    * Property name of the initialisation vector to use, if required, with this
-    * instance. The value associated with this property name is taken to be a
-    * byte array. If the concrete instance needs such a parameter, and it has
-    * not been specified as part of the initialissation parameters, an all-zero
-    * byte array of the appropriate size shall be used.
+    * <p>Property name of the initialisation vector to use, if required, with
+    * this instance. The value associated with this property name is taken to
+    * be a byte array. If the concrete instance needs such a parameter, and it
+    * has not been specified as part of the initialissation parameters, an
+    * all-zero byte array of the appropriate size shall be used.</p>
     */
-   String IV = "gnu.crypto.iv";
+   String IV = "gnu.crypto.mode.iv";
 
-   /** Constant indicating the instance is being used for <i>encryption</i>. */
+   /**
+    * <p>Constant indicating the instance is being used for <i>encryption</i>.</p>
+    */
    int ENCRYPTION = 1;
 
-   /** Constant indicating the instance is being used for <i>decryption</i>. */
+   /**
+    * <p>Constant indicating the instance is being used for <i>decryption</i>.</p>
+    */
    int DECRYPTION = 2;
 
    // Methods
    // -------------------------------------------------------------------------
 
    /**
-    * A convenience method. Effectively invokes the <tt>encryptBlock()</tt> or
-    * <tt>decryptBlock()</tt> method depending on the operational state of the
-    * instance.
+    * <p>A convenience method. Effectively invokes the <code>encryptBlock()</code>
+    * or <code>decryptBlock()</code> method depending on the operational state
+    * of the instance.</p>
     *
     * @param in the plaintext.
-    * @param inOffset index of <i>in</i> from which to start considering data.
+    * @param inOffset index of <code>in</code> from which to start considering
+    * data.
     * @param out the ciphertext.
-    * @param outOffset index of <i>out</i> from which to store result.
+    * @param outOffset index of <code>out</code> from which to store result.
     * @exception IllegalStateException if the instance is not initialised.
     */
    void update(byte[] in, int inOffset, byte[] out, int outOffset)
