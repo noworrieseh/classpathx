8cc8cf9c4c768509ee0617365635c0e833177cc0
Olivier Louchart-Fletcher <olivier@zipworld.com.au>
2002-06-15 06:59:35 +0000

    Fixed DOS characters and added comments.
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@532 814015b8-e43b-4d26-bdac-3fba12e27a77

--- crypto/gcj/admin.sh
+++ crypto/gcj/admin.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 case $1 in
+
+  # Reconfigure the tool chain. This is needed each time configure.in
+  # or the various Makefile.am are updated.
   conf )
     echo 'aclocal && automake && autoconf'
     aclocal && automake && autoconf
     ;;
 
+  # Build the sym links. Automake requires a numbers of files to be
+  # present in all projects. We just sym-link to the files in crypto
+  # root directory.
   links )
     cd source
     test -e gnu || ln -s ../../source/gnu gnu
@@ -17,12 +23,16 @@ case $1 in
     test -e ChangeLog || ln -s ../ChangeLog ChangeLog
     ;;
 
+  # Clean all temporary files. This target should be called before a
+  # CVS check-in.
   clean )
     rm -f NEWS README COPYING AUTHORS ChangeLog *~
     rm -f source/gnu source/*~
     rm -fr autom4te.cache
     ;;
 
+  # Clean all generated files, including configure and Makefile.in makefiles.
+  # Usefull for maintainers.
   cleanall )
     rm -f NEWS README COPYING AUTHORS ChangeLog *~
     rm -f source/gnu source/*~
