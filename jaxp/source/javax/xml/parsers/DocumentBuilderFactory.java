fddf07f9d16d458396154fc2cb6a25dcea9ea101
Arnaud Vandyck <arnaud.vandyck@ulg.ac.be>
2003-08-16 08:49:48 +0000

    JavaDoc changes: getAttribute(String) and setAttribute(String,Object) throws IllegalArgumentException if String is null, newInstance() throws FactoryConfigurationError
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@710 814015b8-e43b-4d26-bdac-3fba12e27a77

--- jaxp/source/javax/xml/parsers/DocumentBuilderFactory.java
+++ jaxp/source/javax/xml/parsers/DocumentBuilderFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DocumentBuilderFactory.java,v 1.7 2001-12-05 22:49:25 db Exp $
+ * $Id: DocumentBuilderFactory.java,v 1.8 2003-08-16 08:49:48 avdyk Exp $
  * Copyright (C) 2001 Andrew Selkirk
  * Copyright (C) 2001 David Brownell
  * 
@@ -43,7 +43,7 @@ import org.xml.sax.*;
  * W3C DOM APIs don't include portable bootstrapping.
  *
  * @author	Andrew Selkirk, David Brownell
- * @version	$Id: DocumentBuilderFactory.java,v 1.7 2001-12-05 22:49:25 db Exp $
+ * @version	$Id: DocumentBuilderFactory.java,v 1.8 2003-08-16 08:49:48 avdyk Exp $
  */
 public abstract class DocumentBuilderFactory {
 
@@ -74,6 +74,9 @@ public abstract class DocumentBuilderFactory {
 	// Methods ----------------------------------------------------
 	//-------------------------------------------------------------
 
+  /**
+   * @exception IllegalArgumentException if implementation doesn't recognize the attribute
+   */
 	public abstract Object getAttribute(String name) 
 		throws IllegalArgumentException;
 
@@ -104,6 +107,9 @@ public abstract class DocumentBuilderFactory {
 	public abstract DocumentBuilder newDocumentBuilder()
 		throws ParserConfigurationException;
 
+  /**
+   * @exception FactoryConfigurationError if the implementation is not available
+   */
 	public static DocumentBuilderFactory newInstance() {
 		try {
 		    return (DocumentBuilderFactory)
@@ -116,6 +122,9 @@ public abstract class DocumentBuilderFactory {
 		}
 	}
 
+  /**
+   * @exception IllegalArgumentException if implementation doesn't recognize the attribute
+   */
 	public abstract void setAttribute(String name, Object value) 
 		throws IllegalArgumentException;
 
