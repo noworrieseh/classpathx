1c43feb979a0c62a95db6abe9ffd2733a8621e7b
Chris Burdess <dog@bluezoo.org>
2006-07-19 09:32:39 +0000

    2006-07-19  Chris Burdess  <dog@bluezoo.org>
    	* source/gnu/mail/handler/Application.java: Permit InputStream as a
    	  supported object type.
    
    
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@1127 814015b8-e43b-4d26-bdac-3fba12e27a77

--- mail/source/gnu/mail/handler/Application.java
+++ mail/source/gnu/mail/handler/Application.java
@@ -124,12 +124,23 @@ public abstract class Application
   public void writeTo(Object object, String mimeType, OutputStream out)
     throws IOException
   {
-    // We only handle arrays of byte
-    byte[] bytes = null;
     if (object instanceof byte[])
-      bytes = (byte[])object;
-    out.write(bytes);
-    out.flush();
+      {
+        byte[] bytes = (byte[]) object;
+        out.write(bytes);
+        out.flush();
+      }
+    else if (object instanceof InputStream)
+      {
+        InputStream in = (InputStream) object;
+        byte[] bytes = new byte[4096];
+        for (int len = in.read(bytes); len != -1; len = in.read(bytes))
+          out.write(bytes, 0, len);
+        out.flush();
+      }
+    else
+      throw new IOException("Unsupported data type: " +
+                            object.getClass().getName());
   }
 
 }
