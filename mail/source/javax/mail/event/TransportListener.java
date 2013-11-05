e6d6182a868432bf522c328a143da0a6bb00946e
Chris Burdess <dog@bluezoo.org>
2013-11-05 09:18:59 +0000

    2013-11-05  Chris Burdess  <dog@gnu.org>
        * mail: Update licence to GPL 3
    
    
    git-svn-id: svn://svn.savannah.gnu.org/classpathx/trunk@1178 814015b8-e43b-4d26-bdac-3fba12e27a77

--- mail/source/javax/mail/event/TransportListener.java
+++ mail/source/javax/mail/event/TransportListener.java
@@ -2,27 +2,22 @@
  * TransportListener.java
  * Copyright (C) 2002 The Free Software Foundation
  *
- * This file is part of GNU JavaMail, a library.
+ * This file is part of GNU Classpath Extensions (classpathx).
+ * For more information please visit https://www.gnu.org/software/classpathx/
  *
- * GNU JavaMail is free software; you can redistribute it and/or modify
+ * classpathx is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * GNU JavaMail is distributed in the hope that it will be useful,
+ * classpathx is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * As a special exception, if you link this library with other files to
- * produce an executable, this library does not by itself cause the
- * resulting executable to be covered by the GNU General Public License.
- * This exception does not however invalidate any other reasons why the
- * executable file might be covered by the GNU General Public License.
+ * along with classpathx.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 package javax.mail.event;
